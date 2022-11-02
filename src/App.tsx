cea146909f75e8fed16bc97f51a9e4cf672615c4
牛叠谊 <18715156450@163.com>
2022-11-02 16:38:49 +0800

    feat: add more examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { i18next, switchLanguage, useTranslation } from './i18n';
+import { i18next, switchLanguage, t, useTranslation } from './i18n';
 
 
 function App() {
@@ -9,12 +9,21 @@ function App() {
       <header className="App-header">
         <div>{t('title',{ns:'pages'})}</div>
         <div>{t('title',{ns:'common'})}</div>
+        <div>{t('color',{ns:'common',defaultValue:'red_en'})}</div>
+        <div>{t('say',{ns:'common',msg:'wtf'})}</div>
+        <br />
+        <button onClick={testFunc}>test</button>
+        <br />
         <button onClick={()=>{
           switchLanguage(i18next.language === 'zh'?"en-US":"zh")
         }}>toggle language</button>
       </header>
     </div>
   );
+}
+
+function testFunc() {
+  alert(t('test',{ns:'common',defaultValue:'test'}))
 }
 
 export default App;
